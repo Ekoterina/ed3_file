a48c2589324382ecc568790db7c16f313b6a3a10
EKOTERINA <ekoterina@elpi-3.elpi3-dlink>
2022-12-05 18:29:13 +0700

    исправила обе функции, но починить первоую так и не смогла

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use std::fs::File;
 use std::fs;
-//use std::io::Read;
+use std::io::Read;
 
 //Функция открывает файл, читает его в строку и возвращает, при неудачной попытке
 //открыть или прочитать файл выполняет проброс ошибки(std::io::Error).
 fn read_file(path: &str) -> Result <String, std::io::Error> {
-    fs::read_to_string(path)
+    let mut text = String::new();
+    File::open(path)?.read_to_string(&mut text)?;
+    Ok(text)
+
+    //fs::read_to_string(path)
 }
 
 //Функция открывает и читает файл в строку, возвращает считанную строку, при ошибках
 //открытия или чтения генерирует панику.
 fn read_file_exact(path: &str) -> String {
-    File::open(path).unwrap();
-    let text = fs::read_to_string(path).unwrap();
-    text
+    //File::open(path).unwrap();
+    //let text = fs::read_to_string(path).unwrap();
+    //text
+
+    fs::read_to_string(path).unwrap()
 }
 
 //Распечатать содержимое файла при помощи функции ‘read_file’, при ошибке вывести
@@ -24,8 +30,8 @@ fn main() {
     let file2 = "text2.txt";
 
     //let text1 = read_file(file);
-    read_file(file);
-    println!("{text1}");
+    //read_file(file);
+    //println!("{text1}");
 
     let text2 = read_file_exact(file2);
     println!("{text2}");
